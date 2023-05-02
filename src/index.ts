9cda9a85b9070b1e3f411ba7787768a3f9d07146
carl beattie <12beattiecastp@gmail.com>
2023-05-02 19:45:11 +0100

    fix: removed rustplus proto file as it is not being used

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 import { onInteraction } from './events/on_interaction';
 import { onReady } from './events/on_ready';
-import fcmListen from './services/rust_plus_pairing';
+import fcmListen, { loadPairedServer } from './services/rust_plus_pairing';
+import RustPlus from './services/rust_plus';
 
 const BOT = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -12,6 +13,25 @@ BOT.once(Events.ClientReady, async client => {
     await onReady(BOT);
 
     fcmListen(BOT);
+
+    const pairedRustServer = loadPairedServer();
+
+    if (pairedRustServer) {
+        const rustPlusConnection = new RustPlus(pairedRustServer.ip, pairedRustServer.port, pairedRustServer.playerId, pairedRustServer.playerToken);
+
+        rustPlusConnection.on('connecting', () => {
+            console.log('Connecting to server:', pairedRustServer.name);
+        })
+
+        rustPlusConnection.on('connected', () => {
+            console.log('Connected to server:', pairedRustServer.name);
+        })
+
+        rustPlusConnection.on('message', (message) => {
+            console.log(message);
+        })
+    }
+
 })
 
 BOT.on(Events.InteractionCreate, async (interaction) => {
